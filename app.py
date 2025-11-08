db121b5ef614c1dec053111e38a171cc0b1f8949
Hawas <hawasbacker@hawass-MacBook-Air.local>
2025-11-08 20:19:57 +0530

    added main block now can be runned with just python app.py

--- app.py
+++ app.py
@@ -3,4 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-	return "<p>Hello human</p>"
+	return "<H1>Hello human</H1>"
+if __name__ == '__main__':
+	app.run(debug=True)
