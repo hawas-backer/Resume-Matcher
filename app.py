414f6464228171ee5ad6f86ebdbfafe706501cc9
Hawas <hawasbacker@hawass-MacBook-Air.local>
2025-11-08 20:23:22 +0530

    tried adding one more route just to test

--- app.py
+++ app.py
@@ -4,5 +4,12 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
 	return "<H1>Hello human</H1>"
+
+@app.route('/test')
+def test():
+	return "<H1>Testing </H!>"
+
+
+
 if __name__ == '__main__':
 	app.run(debug=True)
