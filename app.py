79265a8e8373b8306000d111b185152d4cdd7a17
Hawas <hawasbacker@hawass-MacBook-Air.local>
2025-11-09 22:07:43 +0530

    made the file name unique with uuid4

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from werkzeug.utils import secure_filename
-
+from uuid import uuid4
 import os
 
 app = Flask(__name__)
@@ -12,6 +12,9 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 #ALLOWED_EXTENSIONS = {'txt','pdf'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def make_unique(string):
+	ident = uuid4().hex[:8]
+	return f"{ident}-{string}"
 
 @app.route("/")
 def hello_world():
@@ -26,7 +29,8 @@ def upload_file():
 			flash('NO selected file')
 			return redirect(request.url)
 		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+		unique_filename = make_unique(filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'],unique_filename))
 		return f"File {filename} uploaded succesffuly to {UPLOAD_FOLDER}"
 
 
