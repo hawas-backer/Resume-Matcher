3d35e48120289fce5c285944fbc85982f00ffe3c
Hawas <hawasbacker@hawass-MacBook-Air.local>
2025-11-09 21:55:55 +0530

    file upload made possible

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+from werkzeug.utils import secure_filename
+
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+ALLOWED_EXTENSIONS = {'txt','pdf'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 @app.route("/")
 def hello_world():
-	return "<H1>Hello human</H1>"
+	return render_template("index.html")
 
-@app.route('/test')
-def test():
-	return "<H1>Testing </H!>"
+
+@app.route("/upload",methods =['POST','GET'])
+def upload_file():
+	if request.method == 'POST':
+		file = request.files['file']
+		if file.filename == '':
+			flash('NO selected file')
+			return redirect(request.url)
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+		return f"File {filename} uploaded succesffuly to {UPLOAD_FOLDER}"
+
 
 
 
